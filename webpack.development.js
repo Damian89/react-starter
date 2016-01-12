2f7c4ac74286239d7c2ab42472b6bcd7931a131c
damian <damian@b0x>
2016-01-12 17:49:33 +0100

    Version 1.0

--- webpack.development.js
+++ webpack.development.js
@@ -1,29 +1,35 @@
-var Webpack = require('webpack'),
-    path    = require('path');
+var Webpack = require( 'webpack' );
+var path = require( 'path' );
 
 var Config = {
-  context: path.resolve(__dirname, "src"),
-  entry:   "./main.js",
-  output: {
-    path: path.resolve(__dirname, ""),
-    publicPath : "http://localhost:8080/",
-    filename: "main.bundle.js"
-  },
-  module: {
-    loaders: [
-      { test: /\.(jsx|js)$/,
-        exclude: [ path.resolve(__dirname, "node_modules") ],
-        loader: 'babel'
-      }
-    ]
-  },
-  resolve: {
-    alias: {
-      Utils: path.resolve(__dirname, "source/Utilities"),
-      Interfaces: path.resolve(__dirname, "source/Interfaces")
+    context : path.resolve( __dirname, "app" ),
+    entry : "./main.js",
+    output : {
+        path : path.resolve( __dirname, "public" ),
+        publicPath : "http://localhost:8080/public/",
+        filename : "main.bundle.js"
     },
-    extensions: ["", ".js", ".jsx"]
-  }
+    module : {
+        loaders : [
+            { test : /\.(jsx|js)$/,
+                exclude : [
+                    path.resolve( __dirname, "node_modules" )
+                ],
+                loader : 'babel'
+            }
+        ]
+    },
+    resolve : {
+        alias : {
+            Utils : path.resolve( __dirname, "source/Utilities" ),
+            Interfaces : path.resolve( __dirname, "source/Interfaces" )
+        },
+        extensions : [
+            "",
+            ".js",
+            ".jsx"
+        ]
+    }
 };
 
 module.exports = Config;
